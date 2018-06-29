d3ca786b544ea352151e0c9f078c67939f0d7be3
tranduy12a <tranduyptitt@gmail.com>
2018-06-29 09:07:19 +0700

    create 4 page

---
+++ generated/code/Mageplaza/Blog/Model/TrafficFactory.php
@@ -0,0 +1,45 @@
+<?php
+namespace Mageplaza\Blog\Model;
+
+/**
+ * Factory class for @see \Mageplaza\Blog\Model\Traffic
+ */
+class TrafficFactory
+{
+    /**
+     * Object Manager instance
+     *
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    protected $_objectManager = null;
+
+    /**
+     * Instance name to create
+     *
+     * @var string
+     */
+    protected $_instanceName = null;
+
+    /**
+     * Factory constructor
+     *
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     * @param string $instanceName
+     */
+    public function __construct(\Magento\Framework\ObjectManagerInterface $objectManager, $instanceName = '\\Mageplaza\\Blog\\Model\\Traffic')
+    {
+        $this->_objectManager = $objectManager;
+        $this->_instanceName = $instanceName;
+    }
+
+    /**
+     * Create class instance with specified parameters
+     *
+     * @param array $data
+     * @return \Mageplaza\Blog\Model\Traffic
+     */
+    public function create(array $data = array())
+    {
+        return $this->_objectManager->create($this->_instanceName, $data);
+    }
+}
