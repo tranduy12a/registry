c5948cde9e30a7f9974e7c5080e467176a1ca600
tranduy12a <tranduyptitt@gmail.com>
2018-07-04 14:30:52 +0700

    create table opentechiz

--- app/code/DebugTraceModule/DTrace/Block/YourCustomBlock.php
+++ app/code/DebugTraceModule/DTrace/Block/YourCustomBlock.php
@@ -3,16 +3,19 @@ namespace DebugTraceModule\DTrace\Block;
  
 class YourCustomBlock extends \Magento\Framework\View\Element\Template
 { 
+	protected $_productFactory;
     protected $_productRepository;
 	protected $_product;
+	protected $productRepository;
   
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context, 
         \Magento\Catalog\Model\ProductRepository $productRepository,
-		\Magento\Catalog\Model\ProductFactory $topicFactory,
-        array $data = []
+		\Magento\Catalog\Model\ProductFactory $productFactory,
+        array $data = [];
     ) {
 		$this->productFactory = $productFactory;
+		// $this->_productFactory = $productFactory;
         $this->_productRepository = $productRepository;
         parent::__construct($context, $data);
     }
@@ -33,12 +36,17 @@ class YourCustomBlock extends \Magento\Framework\View\Element\Template
 	    return $productPriceById;
 	}
 
-	public function getPriceBySku($sku)
-	{   
-	    //$sku = 'testing'; //Product sku
-	    $product = $this->_product->create();
-	    $productPriceBySku = $product->loadByAttribute('sku', $sku)->getPrice();
-	    var_dump($productPriceBySku);die;
-	    return $productPriceBySku;
+	// public function getPriceBySku($sku)
+	// {   
+	//     //$sku = 'testing'; //Product sku
+	//     $product = $this->_product->create();
+	//     $productPriceBySku = $product->loadByAttribute('sku', $sku)->getPrice();
+	//     // var_dump($productPriceBySku);die;
+	//     return $productPriceBySku;
+	// }
+	public function getProductBySku($sku)
+	{
+	    $products = $this->productFactory->create();
+    	return $this->productFactory->get($sku);
 	}
 }
--- app/code/DebugTraceModule/DTrace/Plugin/InjectVariablesIntoBlocks.php
+++ app/code/DebugTraceModule/DTrace/Plugin/InjectVariablesIntoBlocks.php
@@ -4,10 +4,16 @@ use Magento\Theme\Block\Html\Footer;
 class InjectVariablesIntoBlocks
 {
     protected $customerSession;
+    protected $_productRepository;
+    protected $_product;
+
     public function __construct(
-        \Magento\Customer\Model\Session $customerSession
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Catalog\Model\ProductRepository $productRepository,
+        \Magento\Catalog\Model\ProductFactory $topicFactory
     )
     {
+        $this->_product = $topicFactory;
         $this->customerSession = $customerSession;
     }
     public function beforeToHtml(Footer $subject)
@@ -15,10 +21,13 @@ class InjectVariablesIntoBlocks
         if ($subject->getNameInLayout() !== 'absolute_footer') {
             return;
         }
+        // $product = $this->_product->create();
+        // $productPriceBySku = $product->loadByAttribute('sku', $sku)->getPrice();
+
+
         $subject->setTemplate('DebugTraceModule_DTrace::absolute_footer.phtml');
         $subject->assign('customer', $this->customerSession->getCustomer());
+       //$subject->assign('product_Factoryy', $productPriceBySku);
     }
-    public function getching(){
-        return "123";
-    }
-}
+
+} 
